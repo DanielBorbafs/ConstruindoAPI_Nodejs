2ce63ae6baab34e62193425001b43a6ce648db13
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2023-01-17 08:06:55 -0300

    Create db.js

---
+++ API_CRUD/db.js
@@ -0,0 +1,7 @@
+const Sequelize = require ('sequelize')
+
+const componenteSequelize = new Sequelize('dbProduto', 'root','',
+{
+    dialect: 'mysql', host: 'localhost'
+});
+module.exports = componenteSequelize
